cc122e2c25ad5dffb96247b89568b9aa2b655fa2
ALexandre <alexandre@digeek.fr>
2023-11-16 09:57:42 +0100

    Style liste films

--- app/src/main/java/com/example/navigation/Films.kt
+++ app/src/main/java/com/example/navigation/Films.kt
@@ -1,8 +1,15 @@
 package com.example.navigation
 
 import android.util.Log
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -11,6 +18,11 @@ import androidx.compose.runtime.collectAsState
 import com.example.navigation.ViewModel.MainViewModel
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import com.example.navigation.Model.TmdbMovie
 
@@ -22,7 +34,9 @@ fun Films(viewModel : MainViewModel, onClick: () -> Unit) {
     }
     val movies by viewModel.movies.collectAsState()
     Log.i("Films", movies.size.toString())
-    LazyColumn () {
+    LazyVerticalGrid (
+        columns = GridCells.Fixed(2),
+    ) {
         items(movies) { movie ->
             Movie(movie)
         }
@@ -38,9 +52,18 @@ fun Films(viewModel : MainViewModel, onClick: () -> Unit) {
 
 @Composable
 fun Movie(movie: TmdbMovie) {
-    AsyncImage(
-        model = "https://image.tmdb.org/t/p/w500/${movie.backdrop_path}",
-        contentDescription = "Ma super image"
-    )
-    Text(text = movie.original_title)
+    Column (
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        AsyncImage(
+            model = "https://image.tmdb.org/t/p/w400/${movie.backdrop_path}",
+            contentDescription = "Ma super image"
+        )
+        Text(text = movie.original_title,
+            fontWeight = FontWeight.Bold,
+            textAlign = TextAlign.Center
+        )
+        Text(text = movie.release_date)
+    }
+
 }
